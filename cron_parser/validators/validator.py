8c67bab8e3ead2e043d129be459449c9f4b2c81e
Kulakov Oleg <oakulako@mts.ru>
2022-08-15 00:48:47 +0300

    added minutes validator, added tests for minutes step validator

--- cron_parser/validators/validator.py
+++ cron_parser/validators/validator.py
@@ -13,7 +13,7 @@ class ValidateResult:
 class Validator:
     __value: str
     __valid: bool = None
-    __reasons: list[Exception | None] = []
+    __reasons: list[Exception | None]
 
     def __init__(self, value: str):
         self.value = value
@@ -26,6 +26,7 @@ class Validator:
         return self.__reasons
 
     def __validate(self) -> None:
+        self.__reasons = []
         check_type_res: ValidateResult = self.__validate_type_expression()
         if not check_type_res.valid:
             self.__valid = False
@@ -35,11 +36,18 @@ class Validator:
         check_len_res: ValidateResult = self.__validate_length_expression()
         if not check_len_res.valid:
             self.__valid = False
-            self.__reasons.append(check_type_res.reason)
+            self.__reasons.append(check_len_res.reason)
+            return
+
+        check_minutes: list[ValidateResult] = [el for el in self.__validate_minutes() if not el.valid]
+        if len(check_minutes) > 0:
+            self.__valid = False
+            self.__reasons += [el.reason for el in check_minutes]
             return
 
         # if pass all checks
-        self.__valid = True
+        if len(self.__reasons) == 0:
+            self.__valid = True
 
     def __validate_type_expression(self) -> ValidateResult:
         if isinstance(self.value, str):
@@ -48,8 +56,27 @@ class Validator:
             return ValidateResult(valid=False, reason=WrongTypeException())
 
     def __validate_length_expression(self) -> ValidateResult:
-        length = len(self.value.split(" "))
+        length = len(self.value.split())
         if length in (5, 6):
             return ValidateResult(valid=True)
         else:
             return ValidateResult(valid=False, reason=WrongLengthException())
+
+    def __validate_minutes(self) -> list[ValidateResult]:
+        result = []
+        minutes = self.value.split()[0]
+        allowed_characters = ['*', '/', '-', ','] + [str(el) for el in range(0, 59)]
+        for symbol in minutes:
+            if symbol not in allowed_characters:
+                result.append(
+                    ValidateResult(
+                        valid=False,
+                        reason=SymbolNotAllowedException(msg=f"Symbol {symbol} not allowed in minutes section")
+                    )
+                )
+        if len(result) == 0:
+            return [ValidateResult(valid=True)]
+        else:
+            return result
+
+
--- tests/test_expression.py
+++ tests/test_expression.py
@@ -8,15 +8,15 @@ from cron_parser.parser import Parser
 @pytest.mark.expression_length
 @pytest.mark.parametrize(
     "expression",
-    (  # correct formats
-         "* * * * *",
-         "* * * * * *",
-         "*/30 * 1-5 * Mon-saT",
-         "*/30 * 1,30 * Mon-saT",
-         "*/30 * 1,30 * 1"
+    (
+            "* * * * *",
+            "* * * * * *",
+            "*/30 * 1-5 * Mon-saT",
+            "*/30 * 1,30 * Mon-saT",
+            "*/30 * 1,30 * 1"
     )
 )
-def test_expression_type_must_be_valid(expression):
+def test_expression_must_be_valid(expression):
     result = Parser(expression)
     assert result.validator.is_valid() is True  # el is valid
     assert len(result.validator.reasons()) == 0  # reasons are not present
@@ -45,7 +45,7 @@ def test_expression_length_must_be_not_valid(expression):
 @pytest.mark.skip(reason="not implemented yet")
 @pytest.mark.expression_content
 @pytest.mark.parametrize("expression", ("*!30 * 15 * Mon-saT", "*/30 * 15 * Mon?saT"))
-def test_expression_length_must_be_not_valid(expression):
+def test_expression_content_must_be_not_valid(expression):
     result = Parser(expression)
     assert result.validator.is_valid() is False  # el not valid
     assert len(result.validator.reasons()) > 0  # reasons are present
@@ -53,4 +53,75 @@ def test_expression_length_must_be_not_valid(expression):
     assert len([el for el in result.validator.reasons() if isinstance(el, WrongLengthException)]) > 0
 
 
+@pytest.mark.expression_content
+@pytest.mark.parametrize(
+    "expression",
+    (
+            "* * * * *",
+            "7 * * * *",
+            "0-37 * * * *",
+            "*/30 * * * *",
+            "1,3,5 * * * *",
+    )
+)
+def test_minutes_must_be_valid(expression):
+    result = Parser(expression)
+    assert result.validator.is_valid() is True  # el not valid
+    assert len(result.validator.reasons()) == 0  # reasons are present
 
+
+@pytest.mark.expression_content
+@pytest.mark.parametrize(
+    "expression",
+    (
+            "a * * * *",
+            "0_37 * * * *",
+            "*!30 * * * *",
+            "1:3,5 * * * *",
+    )
+)
+def test_minutes_must_be_not_valid(expression):
+    result = Parser(expression)
+    assert result.validator.is_valid() is False  # el not valid
+    assert len(result.validator.reasons()) > 0  # reasons are present
+    assert len([el for el in result.validator.reasons() if isinstance(el, SymbolNotAllowedException)]) > 0
+
+
+@pytest.mark.expression_content
+@pytest.mark.parametrize(
+    "expression",
+    (
+            "*/2 * * * * *",
+            "*/3 * * * * *",
+            "*/4 * * * * *",
+            "*/5 * * * * *",
+            "*/6 * * * * *",
+            "*/10 * * * * *",
+            "*/12 * * * * *",
+            "*/15 * * * * *",
+            "*/20 * * * * *",
+            "*/30 * * * * *"
+    )
+)
+def test_minutes_step_must_be_valid(expression):
+    result = Parser(expression)
+    assert result.validator.is_valid() is True  # el not valid
+    assert len(result.validator.reasons()) == 0  # reasons are present
+
+
+@pytest.mark.expression_content
+@pytest.mark.parametrize(
+    "expression",
+    (
+            "*/7 * * * * *",
+            "*/13 * * * * *",
+            "*/19 * * * * *",
+            "*/37 * * * * *",
+            "*/54 * * * * *",
+    )
+)
+def test_minutes_must_be_not_valid(expression):
+    result = Parser(expression)
+    assert result.validator.is_valid() is False  # el not valid
+    assert len(result.validator.reasons()) > 0  # reasons are present
+    assert len([el for el in result.validator.reasons() if isinstance(el, SymbolNotAllowedException)]) > 0
